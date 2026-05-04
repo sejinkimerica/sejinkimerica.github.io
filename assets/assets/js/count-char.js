829994083583dacda7a8eb9c1f7eea00bcffe2e1
sejinkimerica <erica092000@snu.ac.kr>
2026-05-04 07:46:43 +0000

    한글 글자 비율 계산

--- assets/assets/js/count-char.js
+++ assets/assets/js/count-char.js
@@ -12,3 +12,18 @@ for (const ch of text) {
 
 console.log(`${text}`);
 console.log(`'${target}' 글자는 ${count} 번 등장합니다.`);
+
+let hangul = 0;
+let total = 0;
+
+for (const ch of text) {
+    if (ch !== " ") { //공백 문자가 아닌 경우
+        total++; // 전체 글자수에 포함
+        if (ch>= "가" && ch <= "힣") {
+            hangul++;
+        }
+    }
+}
+
+const ratio = (hangul / total) * 100;
+console.log(`한글 글자 비율: ${ratio.toFixed(1)}`);
