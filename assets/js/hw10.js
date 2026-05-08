624d44ad8c100b03ee30bfe07fc210d9734efe57
sejinkimerica <erica092000@snu.ac.kr>
2026-05-08 07:58:48 +0000

    Q2 추가

--- assets/js/hw10.js
+++ assets/js/hw10.js
@@ -1,7 +1,7 @@
 // [숙제10] Javascript 기초 연습
 // 2024-10136 김세진
 
-//Q1
+// Q1
 function classifyEra(year) {
     if (year < 1910) {
         return ('개화기 이전');
@@ -20,4 +20,21 @@ function classifyEra(year) {
 const years = [1908, 1936, 1972, 2025];
 for (year of years) {
     console.log(`${year}년: ${classifyEra(year)}`)
+}
+
+
+// Q2
+const works = ["날개", "오감도", "지주회시", "종생기", "권태"];
+console.log(works.length)
+console.log(works[0])
+console.log(works[works.length - 1])
+
+const titled = works.map(work => `「${work}」`);
+console.log(titled);
+
+const long = works.filter(w => w.length >= 3);
+console.log(long);
+
+for (let i = 0; i < long.length; i++) {
+    console.log(`${i + 1}번째 작품: ${long[i]}`);
 }
