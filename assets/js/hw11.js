e25a956921a66ba0305679f4a7332b6f86a45644
sejinkimerica <erica092000@snu.ac.kr>
2026-05-15 07:08:38 +0000

    숙제11 Q1

---
+++ assets/js/hw11.js
@@ -0,0 +1,10 @@
+// [숙제11] 동적 웹 페이지 구현
+// 2024-10136 김세진
+
+// Q1
+const themeBtn = document.querySelector("#theme-btn");
+const q1Box = document.querySelector("q1-box");
+themeBtn.addEventListener("click", () => { 
+    q1Box.classList.toggle("dark");
+    themeBtn.classList.toggle.textContet = "라이트 모드"
+});
