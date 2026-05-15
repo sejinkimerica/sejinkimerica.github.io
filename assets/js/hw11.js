fb9f45b14b586cc99fef26a9a9b0da822e40a3be
sejinkimerica <erica092000@snu.ac.kr>
2026-05-15 07:27:53 +0000

    오타수정

--- assets/js/hw11.js
+++ assets/js/hw11.js
@@ -3,7 +3,7 @@
 
 // Q1
 const themeBtn = document.querySelector("#theme-btn");
-const q1Box = document.querySelector("q1-box");
+const q1Box = document.querySelector("#q1-box");
 themeBtn.addEventListener("click", () => { 
     q1Box.classList.toggle("dark");
     themeBtn.classList.toggle.textContet = "라이트 모드";
