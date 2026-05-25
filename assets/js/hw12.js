3212c955140ec80f1eb816bccff35218096698d7
sejinkimerica <erica092000@snu.ac.kr>
2026-05-25 05:54:35 +0000

    오타수정

--- assets/js/hw12.js
+++ assets/js/hw12.js
@@ -51,7 +51,7 @@ fetch("/data/nobel-literature.csv")
         data: {
             labels: labels,
             datasets: [{
-                        label: "긍그"
+                        label: "긍그",
                         data: counts,
                         borderColor: "rgba(54, 162, 235, 1)",
                         backgroundColor: "rgba(54, 162, 235, 0.2)", //파랑
